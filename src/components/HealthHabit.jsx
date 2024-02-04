91079ecf4c21693fbcf41e215575bab8ced60861
ocguan <ocguan@ucsd.edu>
2024-02-04 14:01:23 -0800

    Added Edit entry button for tracker.

--- src/components/HealthHabit.jsx
+++ src/components/HealthHabit.jsx
@@ -2,9 +2,15 @@ import { useState } from 'react';
 
 //Create an editable today's entry page
 export default function Entries() {
+  const [editingEntries, setEditingEntries] = useState(false);
   const [weight, setWeight] = useState('');
   const [water, setWater] = useState('');
   const [sleep, setSleep] = useState('');
+
+  //edit entries after clicking the corresponding button
+  function editEntries(e) {
+    setEditingEntries(true);
+  }
 
   function changeWeight(e) {
     setWeight(e.target.value);
@@ -17,17 +23,21 @@ export default function Entries() {
   function changeSleep(e) {
     setSleep(e.target.value);
   }
-
   return (
     <>
-      <input value={weight} onChange={changeWeight} />
-      <p>Weight: {weight}</p>
-      
-      <input value={water} onChange={changeWater} />
-      <p>Water: {water}</p>
+    <p> Today's Entries </p>
 
-      <input value={sleep} onChange={changeSleep} />
+      <p>Weight: {weight}</p>
+      <input value={weight} onChange={changeWeight} disabled={!editingEntries} />
+      
+      <p>Water: {water}</p>
+      <input value={water} onChange={changeWater} disabled={!editingEntries} />
+      
       <p>Sleep: {sleep}</p>
+      <input value={sleep} onChange={changeSleep} disabled={!editingEntries} />
+      <p></p>
+      <button onClick={editEntries}>Edit Today's Entries</button>
+
     </>
   );
 }
