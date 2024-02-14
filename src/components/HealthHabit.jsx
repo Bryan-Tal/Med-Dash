c4fa1a8037e00c19237c6545455f1a450b72cbc8
ocguan <ocguan@ucsd.edu>
2024-02-13 19:57:55 -0800

    Added AddGoal and EditGoal skeleton functions for user

--- src/components/HealthHabit.jsx
+++ src/components/HealthHabit.jsx
@@ -3,14 +3,39 @@ import { useState } from 'react';
 //Create an editable today's entry page
 export default function Entries() {
   const [editingEntries, setEditingEntries] = useState(false);
+  const [addingGoal, setAddingGoal] = useState(false);
+  const [isRepeatable, setRepeatable] = useState(false);
+  const [numberByDate, setNumberByDate] = useState(true);
+  const [editingGoal, setEditingGoal] = useState(false);
   const [weight, setWeight] = useState('');
   const [water, setWater] = useState('');
   const [sleep, setSleep] = useState('');
+  //implement target number by date
 
   //edit entries after clicking the corresponding button
   function editEntries(e) {
     setEditingEntries(true);
   }
+
+//   function StopEditingEntries(e) {
+//     setEditingEntries(false);
+//   }
+
+function addGoal(e) {
+    setAddingGoal(true);
+}
+
+function isRepeating(e) {
+    setRepeatable(e.target.checked);
+}
+
+function isNumByDate(e) {
+    setNumberByDate(e.target.checked);
+}
+
+function editGoal() {
+    setEditingGoal(true);
+}
 
   function changeWeight(e) {
     setWeight(e.target.value);
@@ -23,8 +48,12 @@ export default function Entries() {
   function changeSleep(e) {
     setSleep(e.target.value);
   }
+  //Stop editting feature not used rn: <div onClick={StopEditingEntries}></div>
+
+  // Aesthetic <span style={{ color: habits.exercise[day] >= goal.exercise ? 'green' : 'red' }}>
   return (
     <>
+
     <p> Today's Entries </p>
 
       <p>Weight: {weight}</p>
@@ -36,8 +65,20 @@ export default function Entries() {
       <p>Sleep: {sleep}</p>
       <input value={sleep} onChange={changeSleep} disabled={!editingEntries} />
       <p></p>
+
       <button onClick={editEntries}>Edit Today's Entries</button>
 
+      <button onClick={addGoal}>ADD GOAL</button>
+      <button onClick={editGoal}>Edit/Delete Goal</button>
+      <p></p>
+      {addingGoal && (<label>
+      <input type="checkbox" checked={isRepeatable} onChange={isRepeating} />
+      Goal is {isRepeatable ? 'Repeatable' : 'One Time'}
+      <input type="checkbox" checked={numberByDate} onChange={isNumByDate} />
+      Goal is {isRepeatable ? 'Repeatable' : 'One Time'}
+      </label>
+      )}
+      <p></p>
     </>
   );
 }
